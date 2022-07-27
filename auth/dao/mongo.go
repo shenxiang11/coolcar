1788633b4133d21d568235a9a974f58ba132333b
xiang.shen <xiang.shen@shopee.com>
2022-07-27 17:41:49 +0800

    better test

--- auth/dao/mongo.go
+++ auth/dao/mongo.go
@@ -12,19 +12,19 @@ import (
 const openIDField = "open_id"
 
 type Mongo struct {
-	col   *mongo.Collection
-	genID func() primitive.ObjectID
+	col       *mongo.Collection
+	GenIDFunc func() primitive.ObjectID
 }
 
 func NewMongo(db *mongo.Database) *Mongo {
 	return &Mongo{
-		col:   db.Collection("account"),
-		genID: primitive.NewObjectID,
+		col:       db.Collection("account"),
+		GenIDFunc: primitive.NewObjectID,
 	}
 }
 
 func (m *Mongo) ResolveAccountID(c context.Context, openID string) (string, error) {
-	insertID := m.genID()
+	insertID := m.GenIDFunc()
 	res := m.col.FindOneAndUpdate(c, bson.M{
 		openIDField: openID,
 	}, mgo.SetOnInsert(bson.M{
